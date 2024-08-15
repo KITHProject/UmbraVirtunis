62bc6d4e59360c95f9731ff3cdfab79fd9ac1fa7
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-08-15 13:26:25 +0200

    feat: add comic font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css"
 
 import { GeistSans } from "geist/font/sans"
 import { type Metadata } from "next"
+import localFont from "next/font/local"
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -9,12 +10,17 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 }
 
+const virgil = localFont({
+  src: "../fonts/Virgil.woff2",
+})
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${virgil.className}`}>
+      <body className="bg-[url('/images/background.jpg')] bg-cover">
+        {children}
+      </body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        virgil: ["var(--font-virgil)"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
