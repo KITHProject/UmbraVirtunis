b94a4668b13189af505a400882aeab898e622f51
rynkovski <118986014+rynkovski@users.noreply.github.com>
2024-09-09 11:05:51 +0200

    feat: add more character for planet 3

--- src/constants/characters.ts
+++ src/constants/characters.ts
@@ -14,6 +14,14 @@ import ninja from '@/assets/ninja-man-full.png';
 import ninjawoman from '@/assets/ninja-woman-full.png';
 import NinjaAvatar from '@/assets/ninja-man-avatar.png';
 import NinjaWomanAvatar from '@/assets/ninja-woman-avatar.png';
+import elf from '@/assets/elf-man-full.png';
+import elfwoman from '@/assets/elf-woman-full.png';
+import ElfAvatar from '@/assets/elf-man-avatar.png';
+import ElfWomanAvatar from '@/assets/elf-woman-avatar.png';
+import dragon from '@/assets/dragon-full.png';
+import DragonAvatar from '@/assets/dragon-avatar.png';
+import lizard from  '@/assets/lizard-full.png';
+import LizardAvatar from '@/assets/lizard-avatar.png';
 
 
 export const arrayOfCharacters1 = [
@@ -131,6 +139,72 @@ export const arrayOfCharacters2 = [
     image: orcwoman,
     description: 'Orc females are slightly more agile than the males, with a balance of strength and endurance. They are fierce warriors who use their agility to strike with precision while still holding considerable power.',
     value: 'orc-woman',
+    chartData: [
+      { label: 'Str', value: 9 },
+      { label: 'Agi', value: 5 },
+      { label: 'Spd', value: 6 },
+      { label: 'Int', value: 6.5 },
+      { label: 'End', value: 8.5 },
+    ],
+    modelPath: '/orc.glb',
+  },
+];
+
+export const arrayOfCharacters3 = [
+  {
+    name: 'Elf Man',
+    avatar: ElfAvatar,
+    image: elf,
+    description: 'Elf men are paragons of agility and precision, known for their unmatched skill in archery and swordplay. They blend speed with elegance, their movements precise and deliberate. Though not as physically imposing as other races, their keen senses and deep connection to nature give them an edge in both combat and diplomacy, making them versatile and formidable.',
+    value: 'elf',
+    chartData: [
+      { label: 'Str', value: 6 },
+      { label: 'Agi', value: 9 },
+      { label: 'Spd', value: 8 },
+      { label: 'Int', value: 7 },
+      { label: 'End', value: 6.5 },
+    ],
+    modelPath: '/orc.glb',
+  },
+  {
+    name: 'Elf Woman',
+    avatar: ElfWomanAvatar,
+    image: elfwoman,
+    description: 'Elf women are the epitome of grace and agility, moving with a fluidity that seems almost supernatural. They possess sharp intellect and exceptional skill in magic, making them formidable strategists and spellcasters. While they may lack the raw strength of other races, their speed, precision, and mystical prowess make them deadly opponents in any encounter.',
+
+    value: 'elf-woman',
+    chartData: [
+      { label: 'Str', value: 5.5 },
+      { label: 'Agi', value: 9 },
+      { label: 'Spd', value: 8 },
+      { label: 'Int', value: 7.5 },
+      { label: 'End', value: 7 },
+    ],
+    modelPath: '/orc.glb',
+  },
+  {
+    name: 'Lizard',
+    avatar: LizardAvatar,
+    image: lizard,
+    description: 'Lizardfolk are agile and cunning, thriving in both water and on land. They rely on their quick reflexes and sharp instincts in combat, striking with surprising speed and ferocity. While they may not possess the same raw power as larger creatures, their adaptability, stealth, and resourcefulness make them dangerous and unpredictable adversaries.',
+
+    value: 'lizard',
+    chartData: [
+      { label: 'Str', value: 9.5 },
+      { label: 'Agi', value: 4.5 },
+      { label: 'Spd', value: 5.5 },
+      { label: 'Int', value: 6 },
+      { label: 'End', value: 9 },
+    ],
+    modelPath: '/orc.glb',
+  },
+  {
+    name: 'Dragon',
+    avatar: DragonAvatar,
+    image: dragon,
+    description: 'Dragons are titanic beings of immense power and wisdom, their mere presence instilling awe and fear. They command the skies with their enormous wingspans and unleash devastating breath attacks that can decimate entire armies. While their size might suggest a lack of speed, dragons are surprisingly swift, combining brute strength with ancient magic to dominate any battlefield.',
+
+    value: 'dragon',
     chartData: [
       { label: 'Str', value: 9 },
       { label: 'Agi', value: 5 },
